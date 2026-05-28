c8707bc418c25683c7a8ded5c87ed04f25689f69
Try_si <ezio@elendil.fr>
2026-05-28 19:50:43 +0200

    first commit

---
+++ Input/InputManager.go
@@ -0,0 +1,48 @@
+package input
+
+import "github.com/hajimehoshi/ebiten"
+
+type InputManager struct {
+	Bindings map[string]int
+}
+
+func (im *InputManager) AddBinding(name string, key int) {
+	im.Bindings[name] = key
+}
+
+func (im *InputManager) GetBinding(name string) int {
+	return im.Bindings[name]
+}
+
+func (im *InputManager) SetBinding(name string, key int) {
+	im.Bindings[name] = key
+}
+
+func (im *InputManager) RemoveBinding(name string) {
+	delete(im.Bindings, name)
+}
+
+func (im *InputManager) HasBinding(name string) bool {
+	_, ok := im.Bindings[name]
+	return ok
+}
+
+func (im *InputManager) GetBindings() map[string]int {
+	return im.Bindings
+}
+
+func (im *InputManager) ClearBindings() {
+	im.Bindings = make(map[string]int)
+}
+
+func (im *InputManager) GetBindingCount() int {
+	return len(im.Bindings)
+}
+
+func (im *InputManager) IsKeyPressed(binding string) bool {
+	key, ok := im.Bindings[binding]
+	if !ok {
+		return false
+	}
+	return ebiten.IsKeyPressed(ebiten.Key(key))
+}
